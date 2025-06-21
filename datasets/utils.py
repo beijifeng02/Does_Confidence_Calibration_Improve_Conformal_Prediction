bf589fda503fa315f2db442028ed448bc3c9f2cf
33798 <3379854453@qq.com>
2025-06-22 00:35:50 +0800

    message

--- datasets/utils.py
+++ datasets/utils.py
@@ -1,22 +1,22 @@
 import os
 import torch
-from torchvision import transforms, datasets
+import torchvision
 
 
 # ----------------------------- transform -------------------------------- #
 # imagenet transform
-transform_imagenet_train = transforms.Compose([
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+transform_imagenet_train = torchvision.transforms.Compose([
+    torchvision.transforms.RandomResizedCrop(224),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
 ])
 
-transform_imagenet_test = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+transform_imagenet_test = torchvision.transforms.Compose([
+    torchvision.transforms.Resize(256),
+    torchvision.transforms.CenterCrop(224),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
 ])
 
 # ----------------------------------------------------------------------- #
@@ -24,15 +24,17 @@ transform_imagenet_test = transforms.Compose([
 
 def build_dataloader_imagenet(data_dir, conf_num=5000, temp_num=5000, batch_size=512, num_workers=8):
     validir = os.path.join(data_dir, 'imagenet/images/val')
-    testset = datasets.ImageFolder(root=validir, transform=transform_imagenet_test)
+    testset = torchvision.datasets.ImageFolder(root=validir, transform=transform_imagenet_test)
 
     dataset_length = len(testset)
     cal_num = conf_num + temp_num
     calibset, testset = torch.utils.data.random_split(testset, [cal_num, dataset_length - cal_num])
     conf_calibset, calib_calibset = torch.utils.data.random_split(calibset, [conf_num, cal_num - conf_num])
 
-    calib_calibloader = torch.utils.data.DataLoader(dataset=calib_calibset, batch_size=batch_size, num_workers=num_workers)
-    conf_calibloader = torch.utils.data.DataLoader(dataset=conf_calibset, batch_size=batch_size, num_workers=num_workers)
+    calib_calibloader = torch.utils.data.DataLoader(dataset=calib_calibset, batch_size=batch_size,
+                                                    num_workers=num_workers)
+    conf_calibloader = torch.utils.data.DataLoader(dataset=conf_calibset, batch_size=batch_size,
+                                                   num_workers=num_workers)
     testloader = torch.utils.data.DataLoader(dataset=testset, batch_size=batch_size, num_workers=num_workers)
 
     return calib_calibloader, conf_calibloader, testloader
